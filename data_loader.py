5ab32cde2ca0eb93a4c8f9a522c7b8f9e2227c5c
RajathCA <rajathcaralikatti@gmail.com>
2019-12-25 21:59:24 +0530

    Objectives Updated

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,19 @@
 class DataLoader:
+
     def __init__(self):
+        self.train_X = range(100)
+        self.train_Y = range(100)
+
+    def getBatch(self, batch_size, tag):
+        return self.train_X[:batch_size], self.train_Y[:batch_size]
+
+    def _preprocess(self):
         pass
-    def getBatch(self):
-        pass
+
+data_loader = DataLoader()
+X, Y = data_loader.getBatch(16, "train")
+print "train", X, Y
+X, Y = data_loader.getBatch(16, "test")
+print "test", X, Y
+X, Y = data_loader.getBatch(16, "val")
+print "val", X, Y
--- train.py
+++ train.py
@@ -1,2 +1,73 @@
-from models.feedforward import feedforward
+#from models.feedforward import feedforward
 from data_loader import DataLoader
+
+# Define placeholders (inputs and outputs given to model)
+
+
+
+
+
+# Define model (graph)
+# Start with feedforward model
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Define Cross-Entropy Loss
+
+
+
+
+# Define optimizer
+# and the optimize operation
+
+
+
+
+# Define session and initialize weights
+
+
+
+
+# For training, implement the following using DataLoader Object
+# Run training MAX_EPOCHS times
+# In every epoch, cycle through all training data randomly
+# After every epoch, save model weights in folder model_weights using tf.train.Saver()
+# After every epoch, compute avg. loss on validation set
+# Stop training once validation cost starts increasing
+# Split entire training data into train (90%) and val (10%) sets
+
+MAX_EPOCHS = 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Load weights using tf.train.Saver() from epoch with lowest validation cost.
+# Compute avg. test accuracy and loss
+
+
+
+
+
+
