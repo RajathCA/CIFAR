b76f37540000ad1348125aeeb0a4b450c29bff7f
RajathCA <rajathcaralikatti@gmail.com>
2020-01-05 19:35:38 +0530

    data_loader.py updated and completed

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,89 @@
+train_percent = 0.9
+val_percent = 1 - train_percent
+num_channels = 3
+img_size = 32
+
+import numpy as np
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding = 'bytes')
+    return dict
+
+def get_img_and_label(file):
+    dict = unpickle(file)
+    raw_img = dict[b'data']
+    raw_float = np.array(raw_img, dtype = np.float32) / 255.0
+    images = raw_float.reshape([-1, num_channels, img_size, img_size])
+    images = images.transpose([0, 2, 3, 1])
+    labels = dict[b'labels']
+    return images, labels
+
+def simultaneous_shuffle(images, labels):
+    num_images = images.shape[0]
+    shuffle_ind = np.arange(num_images)
+    np.random.shuffle(shuffle_ind)
+    images = images[shuffle_ind]
+    labels = labels[shuffle_ind]
+    return images, labels
+
+def one_hot_encode(labels):
+    one_hot_labels = np.zeros((len(labels), 10), dtype = np.int32)
+    one_hot_labels[np.arange(len(labels)), labels] = 1
+    return one_hot_labels
+
+def get_train_and_val():
+    images_all, labels_all = get_img_and_label('data\\cifar-10-batches-py\\data_batch_1')
+    for x in range(2, 6):
+        file = 'data\\cifar-10-batches-py\\data_batch_' + str(x)
+        images, labels = get_img_and_label(file)
+        images_all = np.concatenate((images_all, images))
+        labels_all = np.concatenate((labels_all, labels))
+    images_all, labels_all = simultaneous_shuffle(images_all, labels_all)
+    num_images_all = images_all.shape[0]
+    num_images_train = int (num_images_all * train_percent)
+    images_train = images_all[:num_images_train]
+    images_val = images_all[num_images_train:]
+    labels_train = labels_all[:num_images_train]
+    labels_val = labels_all[num_images_train:]
+    return images_train, one_hot_encode(labels_train), images_val, one_hot_encode(labels_val)
+
+def get_test():
+    images_test, labels_test = get_img_and_label('data\\cifar-10-batches-py\\test_batch')
+    num_images_test = images_test.shape[0]
+    return images_test, one_hot_encode(labels_test)
+
 class DataLoader:
 
     def __init__(self):
-        self.train_X = range(100)
-        self.train_Y = range(100)
+        self.train_X , self.train_Y, self.val_X, self.val_Y = get_train_and_val()
+        self.test_X, self.test_Y = get_test()
+        self.train_ind, self.val_ind, self.test_ind = 0, 0 , 0
+        self.n_train, self.n_val, self.n_test = self.train_X.shape[0], self.val_X.shape[0], self.test_X.shape[0]
+    def get_batch(self, batch_size, tag):
+        if tag == 'train':
+            X, Y = self.train_X[self.train_ind : self.train_ind + batch_size], self.train_Y[self.train_ind : self.train_ind + batch_size]
+            self.train_ind += batch_size
+        elif tag == 'val':
+            X, Y = self.val_X[self.val_ind : self.val_ind + batch_size], self.val_Y[self.val_ind : self.val_ind + batch_size]
+            self.val_ind += batch_size
+        elif tag == 'test':
+            X, Y = self.test_X[self.test_ind : self.test_ind + batch_size], self.test_Y[self.test_ind : self.test_ind + batch_size]
+            self.test_ind += batch_size
+        return X, Y
 
-    def getBatch(self, batch_size, tag):
-        return self.train_X[:batch_size], self.train_Y[:batch_size]
+    def reset_epoch(self):
+        self.train_ind, self.val_ind, self.test_ind = 0, 0 , 0
+        self.train_X , self.train_Y = simultaneous_shuffle(self.train_X , self.train_Y)
 
-    def _preprocess(self):
-        pass
-
-data_loader = DataLoader()
-X, Y = data_loader.getBatch(16, "train")
-# X - [batch_size, width, height, channels]
-# Y - [batch_size, num_classes]
-print "train", X, Y
-X, Y = data_loader.getBatch(16, "test")
-print "test", X, Y
-X, Y = data_loader.getBatch(16, "val")
-print "val", X, Y
+if __name__ == "__main__":
+    data_loader = DataLoader()
+    # X - [batch_size, height, width, channels]
+    # Y - [batch_size, num_classes]
+    X, Y = data_loader.get_batch(16, "train")
+    print ("train", X, Y)
+    X, Y = data_loader.get_batch(16, "test")
+    print ("test", X, Y)
+    X, Y = data_loader.get_batch(16, "val")
+    print ("val", X, Y)
